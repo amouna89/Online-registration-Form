2f6211bbc3c481fa0f1ec38fa026d8bc5a334bca
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-09-22 15:47:24 -0500

    add comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 /*Form valadation*/
- 
-/*valadating the first name*/ 
+
+/*valadating the first name*/
+
+//test(); to test if the string matches the pattern
+//replace(); to replace a string expample : formating a phone number 
 
